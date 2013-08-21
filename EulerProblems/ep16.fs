dc92309f5808631e665411d2b4d98a0e47770bef
Grzegorz Gierlik <ggierlik+github@gazeta.pl>
2013-08-21 20:05:06 +0200

    Solved ep16

---
+++ EulerProblems/ep16.fs
@@ -0,0 +1,13 @@
+﻿module ep16
+//http://projecteuler.net/problem=16
+
+open System.Numerics
+
+let powersOf2 = Seq.unfold(fun i -> Some(2I**i, i+1)) (0)
+
+let solve n = 
+    let v = powersOf2 |> Seq.nth n |> (fun x -> x.ToString())
+//    printfn "%s" v
+    let sum = v |>Seq.map(fun c -> int c - int '0') |> Seq.sum
+//    printfn "%d" sum
+    sum
