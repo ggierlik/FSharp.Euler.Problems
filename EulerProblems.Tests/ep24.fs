e13a88d074b297f5a865f90f131f9bd64b83b770
Grzegorz Gierlik <ggierlik+github@gazeta.pl>
2013-11-30 20:38:06 +0100

    on the way with Euler Problem 24

---
+++ EulerProblems.Tests/ep24.fs
@@ -0,0 +1,29 @@
+﻿module ep24
+
+open ep24
+open NUnit.Framework
+open FsUnit
+
+[<Test>]
+let ``Test Lists`` () =
+    [0; 1; 2] |> should equal [0; 1; 2];
+    [0; 1; 2] |> should not' (be equal [0; 1]);
+    [] |> should equal [];
+    (1, [1; 2; 3]) |> should equal (1, [1; 2; 3])
+    (1, []) |> should equal (1, [])
+    (1, [], [1; 2; 3]) |> should equal (1, [], [1; 2; 3])
+
+[<Test>]
+let ``Test get_longest_tail`` () =
+    get_longest_tail [0..3] |> should equal (2, [0; 1], [3])
+    get_longest_tail [0; 1; 3; 2] |> should equal (1, [0], [3; 2])
+    get_longest_tail [0; 3; 2; 1] |> should equal (0, [], [3; 2; 1])
+    get_longest_tail [2; 3; 1; 0] |> should equal (2, [], [3; 1; 0])
+    get_longest_tail [4; 5; 2; 3; 1; 0] |> should equal (2, [4; 5], [3; 1; 0])
+
+[<Test>]
+let ``Test get_first_greater`` () =
+    get_first_greater [0; 1; 2; 3] 2 |> should equal 3
+    get_first_greater [0; 1; 2; 3] 0 |> should equal 1
+    get_first_greater [3; 1; 0] 2 |> should equal 3
+    (fun () -> get_first_greater [0; 1; 2; 3] 3 |> ignore) |> should throw typeof<System.ArgumentException>
--- EulerProblems/ep24.fs
+++ EulerProblems/ep24.fs
@@ -1,4 +1,60 @@
 ﻿module ep24
 //http://projecteuler.net/problem=24
 
+(*
+number of permutations on n elements set is n!
+9!      = 362880
+9!*2    = 725760
 
+1.       0123456789
+725761.  2013456789
+
+10^6-2*9!   = 274240
+8!          = 40320
+
+274240 / 40320  = 6.8
+40320 * 6       = 241920
+
+725760 + 241920 = 967680
+
+967680. 2013
+0123
+0132
+01 23
+02 13
+0213
+0231
+
+*)
+
+let perm = [0..9]
+
+let get_longest_tail lst = 
+
+    let rec get_tail acc lst =
+        printfn "%A %A" acc lst
+
+        let h = List.head lst    
+        let e = List.head acc
+
+        printfn "%d %d" h e
+
+        if e < h then get_tail (h::acc) (List.tail lst)
+        else (h, lst |> List.tail |> List.rev, acc)
+
+    let l = List.rev lst
+    get_tail [List.head l] (List.tail l)
+
+let get_first_greater lst elem = 
+    lst |> List.filter (fun e -> e > elem) |> List.head
+
+let next_perm perm =
+    
+    let e, tail, lst = get_longest_tail perm
+
+    printfn "%d %A" e tail
+
+    let g = get_first_greater tail e
+
+    perm
+            
