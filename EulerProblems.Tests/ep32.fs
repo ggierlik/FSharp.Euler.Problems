9149b8fc8b8aa9a72abf9b5d09ef10cfda75054c
Grzegorz Gierlik <ggierlik+github@gazeta.pl>
2014-05-02 00:04:32 +0200

    first try

---
+++ EulerProblems.Tests/ep32.fs
@@ -0,0 +1,12 @@
+﻿module ep32
+
+open ep32
+open NUnit.Framework
+open FsUnit
+
+[<Test>]
+let ``Test is_pandigital`` () =
+    is_pandigital 82 93 |> should equal true
+    is_pandigital 3 4 |> should equal true
+    is_pandigital 7 8 |> should equal true
+    is_pandigital 7 7 |> should equal false
--- EulerProblems/ep32.fs
+++ EulerProblems/ep32.fs
@@ -7,3 +7,30 @@
 let ns = seq {1..98}
 
 let ms n = Seq.unfold (fun i -> Some (i, i+1)) (n)
+
+let is_pandigital n m =
+    let all = "123456789".ToCharArray() |> Set.ofArray
+
+    let makeSet n =
+        n |> string |> fun s -> s.ToCharArray() |> Array.toList |> Set.ofList
+
+    let sn = makeSet n
+    let sm = makeSet m
+    let sp = makeSet (n*m)
+
+    let seqOfSets = [sn; sm; sp] |> List.toSeq
+    
+//    Set.intersect
+//    Set.intersectMany
+
+    //seqOfSets
+    let r1 = Set.unionMany seqOfSets
+
+    let diff = Set.intersect all r1
+    let union = Set.union all r1
+
+    true
+
+
+
+
