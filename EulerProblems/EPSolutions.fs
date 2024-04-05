daa4879588c813681256d53ba0e8fbeac20a74f3
Grzegorz Gierlik <ggierlik@gmail.com>
2024-04-05 21:04:41 +0200

    Uncomments all calls

--- EulerProblems/EPSolutions.fs
+++ EulerProblems/EPSolutions.fs
@@ -6,81 +6,86 @@ open System
 
 let ts = System.Diagnostics.Stopwatch.StartNew()
 
-//ep1.solve |> printfn "%d"
-//ep2.solve |> printfn "%d"
-//ep3.solve |> printfn "%d"
+ep1.solve |> printfn "%d"
+ep2.solve |> printfn "%d"
+ep3.solve |> printfn "%d"
 
-//ep4.solve |> printfn "%d"
-//ep5.solve |> printfn "%d"
-//ep6.solve |> printfn "%d"
+ep4.solve |> printfn "%d"
+ep5.solve |> printfn "%d"
+ep6.solve |> printfn "%d"
 
-//ep7_10.solve_ep7 |> printfn "%d"
-////printfn "Time %fms" ts.Elapsed.TotalMilliseconds
-//ts.Reset()
+printfn "EP1-6 Time %fms" ts.Elapsed.TotalMilliseconds
 
-//ep8.solve |> printfn "%d"
-//ep9.solve |> printfn "%d"
+ts.Restart()
+ep7_10.solve_ep7 |> printfn "%d"
+printfn "EP7 Time %fms" ts.Elapsed.TotalMilliseconds
+ts.Restart()
 
-//ep7_10.solve_ep10 |> printfn "%d"
-////printfn "Time %fms" ts.Elapsed.TotalMilliseconds
-//ts.Reset()
+ep8.solve |> printfn "%d"
+ep9.solve |> printfn "%d"
 
-//ep11.solve |> printfn "%d"
+ts.Restart()
+ep7_10.solve_ep10 |> printfn "%d"
+printfn "EP10 Time %fms" ts.Elapsed.TotalMilliseconds
+ts.Restart()
 
-//ep12.solve |> printfn "%d"
+ep11.solve |> printfn "%d"
 
-//ep13.solve |> printfn "%s"
+ep12.solve |> printfn "%d"
 
-//ep14.solve1 |> printfn "%d"
-//printfn "Time %fms" ts.Elapsed.TotalMilliseconds
-//ts.Reset()
+ep13.solve |> printfn "%s"
 
-//ep14.solve2 |> printfn "%d"
-//printfn "Time %fms" ts.Elapsed.TotalMilliseconds
-//ts.Reset()
+ts.Restart()
+ep14.solve1 |> printfn "%d"
+printfn "EP14.1 Time %fms" ts.Elapsed.TotalMilliseconds
+ts.Restart()
 
-//ep15.solve 20I |> printfn "%A"
+ep14.solve2 |> printfn "%d"
+printfn "EP14.2 Time %fms" ts.Elapsed.TotalMilliseconds
+ts.Restart()
 
-//ep16.solve 1000 |> printfn "%d"
+ep15.solve 20I |> printfn "%A"
 
-//ep17.solve 1000 |> printfn "%d"
+ep16.solve 1000 |> printfn "%d"
 
-//ep18.solve @"ep18.in" |> printfn "%d"
-//ep18.solve @"ep67.in" |> printfn "%d"
+ep17.solve 1000 |> printfn "%d"
 
-//ep19.solve_1 () |> printfn "%d"
+ep18.solve @"ep18.in" |> printfn "%d"
+ep18.solve @"ep67.in" |> printfn "%d"
 
-//ep20.solve 100 |> printfn "%d"
+ep19.solve_1 () |> printfn "%d"
 
-//ep21.solve () |> printfn "%d"
+ep20.solve 100 |> printfn "%d"
 
-//ep22.solve () |> printfn "%d"
+ep21.solve () |> printfn "%d"
 
-//ep23.solve () |> printfn "%d"
+ep22.solve () |> printfn "%d"
 
-//ep24.solve 1000000 [0..9] |> printfn "%s"
+ep23.solve () |> printfn "%d"
 
-//ep25.solve 1000 |> printfn "%A"
+ep24.solve 1000000 [0..9] |> printfn "%s"
 
-//ep27.solve () |> printfn "%d"
+ep25.solve 1000 |> printfn "%A"
 
-//ep28.solve 1001 |> printfn "%d"
+ep27.solve () |> printfn "%d"
 
-//ep29.solve |> printfn "%d"
+ep28.solve 1001 |> printfn "%d"
 
-//ep30.solve 5 |> printfn "%d"
+ep29.solve |> printfn "%d"
 
-//ep31.solve 200 |> printfn "%d"
+ep30.solve 5 |> printfn "%d"
 
-//ep32.solve |> printfn "%d"
+ep31.solve 200 |> printfn "%d"
 
-//ep34.solve |> printfn "%d"
+ep32.solve |> printfn "%d"
 
-//ep35.solve 1000000 |> printfn "%d"
+ep34.solve |> printfn "%d"
+
+ep35.solve 1000000 |> printfn "%d"
 
 ep36.solve 999999 |> printfn "%d"
 
-printfn "Time %fs" ts.Elapsed.TotalSeconds
+printfn "Total time %fs" ts.Elapsed.TotalSeconds
 ts.Reset()
 
 Console.ReadKey() |> ignore
